3e72336681b59c2e48b1abf1ed73d1dd551c48b4
João Gonçalves <heavyjg@gmail.com>
2023-12-27 21:41:38 -0300

    refactor Event type and table

--- src/events/types.ts
+++ src/events/types.ts
@@ -1,6 +1,15 @@
 export const EVENT_KEY = process.env.EVENTS_TABLE as string;
 
 export type Event = {
-  eventId: string;
-  name: string;
+  eventId: string; // Unique identifier for the event
+  eventName: string; // Name of the event
+  eventType: string; // Type of the event (concert, party, show, dinner, etc.)
+  eventDate: string; // Date and time of the event (ISO 8601 format recommended)
+  location: string; // Location of the event
+  host: string; // Host of the event
+  description?: string; // Optional brief description of the event
+  capacity?: number; // Optional maximum number of attendees
+  ticketPrice?: number; // Optional ticket price for the event
+  tags?: string[]; // Optional list of tags or keywords associated with the event
+  status?: string; // Optional current status of the event (scheduled, cancelled, etc.)
 };
